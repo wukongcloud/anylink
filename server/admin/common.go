2154c0096fc9586c372400e6fef7a7f129db9fcf
SherwinWang <sherwinwangs@hotmail.com>
2024-03-01 23:26:48 +0400

    Update common.go

--- server/admin/common.go
+++ server/admin/common.go
@@ -73,7 +73,8 @@ func SendMail(subject, to, htmlBody string) error {
 	// - PLAIN (default)
 	// - LOGIN
 	// - CRAM-MD5
-	server.Authentication = mail.AuthPlain
+	// server.Authentication = mail.AuthPlain
+	server.Authentication = mail.AuthLogin
 
 	// Variable to keep alive connection
 	server.KeepAlive = false
